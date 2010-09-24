2776704ede8732d14c0b6db92bc47caf8dff0755
Jay Levitt <jay@jay.fm>
2010-09-24 11:19:48 -0400

    switch server to Sinatra

--- idle_server.rb
+++ idle_server.rb
@@ -1,40 +1,15 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'gserver'
-require 'trollop'
+require 'sinatra'
 
-class IdleServer < GServer
-  def initialize(*args)
-    super(*args)
+$idle_seconds = 0
 
-    @@idle_seconds = 0
-  end
-
-  def serve(io)
-    io.puts("passthrough_idle server 0.1")
-
-    loop do
-      line = io.readline
-      case line
-      when /PULL/i 
-        io.puts "Last known idle time: #{@@idle_seconds}"
-      when /SET ([0-9]+)/i
-        @@idle_seconds = $1
-        io.puts "Idle time set: #{@@idle_seconds}"
-      when /QUIT/i
-        break
-      else
-        io.puts "Unknown command"
-      end
-    end
-
-  end
+get '/' do
+  "Last known idle time: #{$idle_seconds}\n"
 end
 
-opts = Trollop::options do
-  opt :port, "idle server port", :type => :int, :default => 12389
+put '/:idle' do
+  $idle_seconds = params[:idle]
+  "Idle time set: #{$idle_seconds}\n"
 end
-
-server = IdleServer.new(opts[:port], "0.0.0.0")
-server.start
-server.join
+  
